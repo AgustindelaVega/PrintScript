99d6791eb814ae9865cc6e52b05d314f0cb2bff0
Braian Aniel Biale <braian.biale@ing.austral.edu.ar>
2021-04-08 21:15:56 -0300

    add cli implementation

--- app/src/main/java/edu/austral/ingsis/CLI.java
+++ app/src/main/java/edu/austral/ingsis/CLI.java
@@ -1,19 +1,40 @@
 package edu.austral.ingsis;
 
+import edu.austral.ingsis.exception.ParseException;
 import edu.austral.ingsis.parser.Parser;
 import edu.austral.ingsis.parser.impl.PrintScriptParser;
+import edu.austral.ingsis.statement.Statement;
 import edu.austral.ingsis.token.Token;
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.Callable;
+import picocli.CommandLine;
 
 public class CLI implements Callable<Integer> {
 
-  Lexer lexer = new PrintScriptLexer();
-  Parser parser = new PrintScriptParser();
-  List<Token> tokens = lexer.lex("./src/main/resources/file.txt");
+  @CommandLine.Option(
+      names = {"-f", "--file"},
+      description = "Archive file")
+  File archive;
+
+  private final Lexer lexer = new PrintScriptLexer();
+  private final Parser parser = new PrintScriptParser();
+  // private final Interpreter interpreter = new PrintScriptInterpreter();
 
   @Override
   public Integer call() throws Exception {
-    return null;
+    List<Token> tokens;
+    List<Statement> statements;
+
+    try {
+      tokens = lexer.lex(archive.getPath());
+      statements = parser.parse(tokens);
+      // interpreter.interpret(statements);
+    } catch (LexerException | ParseException e) {
+      e.printStackTrace();
+      return 1;
+    }
+
+    return 0;
   }
 }
